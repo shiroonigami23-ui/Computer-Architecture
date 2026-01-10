7d375bef96cf378508c126117b2f19345c7b1011
ShiroOni23 <shiroonigami23@gmail.com>
2026-01-11 02:04:34 +0530

    Update auth_manager.js

--- auth_manager.js
+++ auth_manager.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDSvQT6Zd-vX6T9qK5_y_kYCg7epRrgYQ0",
+  apiKey: "fire",
   authDomain: "computer-architecure-b52ab.firebaseapp.com",
   projectId: "computer-architecure-b52ab",
   storageBucket: "computer-architecure-b52ab.firebasestorage.app",
